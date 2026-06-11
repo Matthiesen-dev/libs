885b2c227fcb3b0b5f127d94ac0745d7c6693aa9
Adam Matthiesen <amatthiesen@outlook.com>
2026-06-11 15:50:11 -0700

    refactor publishing conventions to set project version dynamically and improve repository configuration

--- gradle/build-logic/src/main/kotlin/matthiesen.publishing-conventions.gradle.kts
+++ gradle/build-logic/src/main/kotlin/matthiesen.publishing-conventions.gradle.kts
@@ -1,4 +1,5 @@
 import com.vanniktech.maven.publish.MavenPublishBaseExtension
+import org.gradle.api.Project
 import org.gradle.api.credentials.PasswordCredentials
 import org.gradle.api.publish.PublishingExtension
 import org.gradle.authentication.http.BasicAuthentication
@@ -8,51 +9,67 @@ plugins {
     id("com.vanniktech.maven.publish")
 }
 
-configure<PublishingExtension> {
-    repositories {
-        maven {
-            name = "devMatthiesenMaven"
-            url = uri(if (version.toString().endsWith("SNAPSHOT"))
-                "https://maven.matthiesen.dev/snapshots"
-            else "https://maven.matthiesen.dev/releases")
-            credentials(PasswordCredentials::class)
-            authentication {
-                create<BasicAuthentication>("basic")
+// Seed an initial version when the project has not set one yet.
+if (project.version.toString() == Project.DEFAULT_VERSION) {
+    val moduleVersionKey = "${project.name.replace('-', '_')}_version"
+    val moduleVersionEnvKey = moduleVersionKey.uppercase()
+    val resolvedVersion = providers.gradleProperty(moduleVersionKey)
+        .orElse(providers.environmentVariable(moduleVersionEnvKey))
+        .orElse(providers.gradleProperty("version"))
+        .orElse(providers.environmentVariable("PROJECT_VERSION"))
+
+    resolvedVersion.orNull?.let { project.version = it }
+}
+
+afterEvaluate {
+    val publishVersion = project.version.toString()
+
+    configure<PublishingExtension> {
+        repositories {
+            maven {
+                name = "devMatthiesenMaven"
+                url = uri(if (publishVersion.endsWith("SNAPSHOT"))
+                    "https://maven.matthiesen.dev/snapshots"
+                else "https://maven.matthiesen.dev/releases")
+                credentials(PasswordCredentials::class)
+                authentication {
+                    create<BasicAuthentication>("basic")
+                }
             }
         }
     }
-}
 
-configure<MavenPublishBaseExtension> {
-    coordinates(
-        project.group.toString(),
-        project.name,
-        project.version.toString()
-    )
+    configure<MavenPublishBaseExtension> {
+        coordinates(
+            project.group.toString(),
+            project.name,
+            publishVersion
+        )
 
-    pom {
-        name.set(project.name)
-        description.set(project.property("description").toString())
-        inceptionYear.set("2026")
-        url.set(project.property("github_url").toString())
-        licenses {
-            license {
-                name.set(project.property("license").toString())
-                url.set(project.property("license_url").toString())
-                distribution.set(project.property("license_url").toString())
-            }
-        }
-        developers {
-            developer {
-                id.set(project.property("author_id").toString())
-                name.set(project.property("author").toString())
-                url.set(project.property("author_url").toString())
-            }
-        }
-        scm {
+        pom {
+            name.set(project.name)
+            description.set(project.property("description").toString())
+            inceptionYear.set("2026")
             url.set(project.property("github_url").toString())
-            connection.set("scm:git:git://${project.property("git_url").toString()}")
-            developerConnection.set("scm:git:ssh://git@${project.property("git_url").toString()}")
+            licenses {
+                license {
+                    name.set(project.property("license").toString())
+                    url.set(project.property("license_url").toString())
+                    distribution.set(project.property("license_url").toString())
+                }
+            }
+            developers {
+                developer {
+                    id.set(project.property("author_id").toString())
+                    name.set(project.property("author").toString())
+                    url.set(project.property("author_url").toString())
+                }
+            }
+            scm {
+                url.set(project.property("github_url").toString())
+                connection.set("scm:git:git://${project.property("git_url").toString()}")
+                developerConnection.set("scm:git:ssh://git@${project.property("git_url").toString()}")
+            }
         }
     }
 }
